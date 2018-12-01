7c0c9993e6edc23b3100c5936ca5811c6b2def91
Alex "Splitz" Davis <32062643+Splitzy@users.noreply.github.com>
2018-12-01 18:10:10 +0000

    Created an enemy dog hazard

---
+++ PlymHack/Assets/_Scripts/EvasiveManeuver.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvasiveManeuver : MonoBehaviour
+{
+    public float dodge;
+    public float smoothing;
+    public Vector2 startWait;
+    public Vector2 maneuverWait;
+    public Vector2 maneuverTime;
+    public Boundary boundary;
+
+    private float currentSpeed;
+    private float targetManeuver;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        currentSpeed = rb.velocity.z;
+        StartCoroutine(Evade());
+    }
+
+    IEnumerator Evade()
+    {
+        yield return new WaitForSeconds(Random.Range(startWait.x, startWait.y));
+        
+        while(true)
+        {
+            targetManeuver = Random.Range(1, dodge) * Mathf.Sign(transform.position.x);
+            yield return new WaitForSeconds(Random.Range(maneuverTime.x, maneuverTime.y));
+            targetManeuver = 0;
+            yield return new WaitForSeconds(Random.Range(maneuverWait.x, maneuverWait.y));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float newManeuver = Mathf.MoveTowards(rb.velocity.x, targetManeuver, Time.deltaTime * smoothing);
+        rb.velocity = new Vector2(newManeuver, currentSpeed);
+        rb.position = new Vector2(
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            Mathf.Clamp(rb.position.y, boundary.yMin, boundary.yMax)
+            );
+    }
+
+}
