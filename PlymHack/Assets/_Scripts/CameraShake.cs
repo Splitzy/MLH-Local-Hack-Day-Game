1e5745d14c6458494fd51c0ca9e738fb5d005447
Alex "Splitz" Davis <32062643+Splitzy@users.noreply.github.com>
2018-12-01 19:45:30 +0000

    Added a camera shake script for hitting enemies and player

---
+++ PlymHack/Assets/_Scripts/CameraShake.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    Vector3 cameraInitialPos;
+    public float shakeMagnitude = 0.05f, shakeTime = 0.5f;
+    public Camera mainCamera;
+
+    public void ShakeIt()
+    {
+        cameraInitialPos = mainCamera.transform.position;
+        InvokeRepeating("StartCameraShaking", 0f, 0.005f);
+        Invoke("StopCameraShaking", shakeTime);
+    }
+
+    void StartCameraShaking()
+    {
+        float cameraShakingOffsetX = Random.value * shakeMagnitude * 2 - shakeMagnitude;
+        float cameraShakingOffsetY = Random.value * shakeMagnitude * 2 - shakeMagnitude;
+        Vector3 cameraIntermediatePos = mainCamera.transform.position;
+        cameraIntermediatePos.x = cameraShakingOffsetX;
+        cameraIntermediatePos.y = cameraShakingOffsetY;
+        mainCamera.transform.position = cameraIntermediatePos;
+    }
+
+    void StopCameraShaking()
+    {
+        CancelInvoke("StartCameraShaking");
+        mainCamera.transform.position = cameraInitialPos;
+    }
+
+	
+}
