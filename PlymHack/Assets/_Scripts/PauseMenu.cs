e1ae38440e231f00b56479ea8a7c23af9d721bea
Alex "Splitz" Davis <32062643+Splitzy@users.noreply.github.com>
2018-12-01 20:27:57 +0000

    Fixed issues with Main and Pause menu scripts

--- PlymHack/Assets/_Scripts/PauseMenu.cs
+++ PlymHack/Assets/_Scripts/PauseMenu.cs
@@ -1,16 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PauseMenu : MonoBehaviour {
+public class PauseMenu : MonoBehaviour
+{
+    private bool paused = false;
+    public GameObject pauseMenuUI;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused == true)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+        
+    }
+
+    public void Return()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Resume()
+    {
+        paused = false;
+        Time.timeScale = 1f;
+        pauseMenuUI.SetActive(false);
+    }
+
+    public void Pause()
+    {
+        paused = true;
+        Time.timeScale = 0f;
+        pauseMenuUI.SetActive(true);
+    }
 }
